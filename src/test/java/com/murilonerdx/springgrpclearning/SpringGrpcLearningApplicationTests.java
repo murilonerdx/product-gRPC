9893ce1f86c36fa707e3f71dcb022cdd808af9aa
Murilo P.S <mu-silva@outlook.com>
2022-07-31 19:13:22 -0300

    Adicionando projeto inicial

---
+++ src/test/java/com/murilonerdx/springgrpclearning/SpringGrpcLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.springgrpclearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
